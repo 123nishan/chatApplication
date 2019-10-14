9182e4aeec51e25adb5cf435bfd229140eda3166
123nishan <30835894+123nishan@users.noreply.github.com>
2019-10-14 19:26:17 +0545

    Store user send message to database to with send and receiver detail

--- app/src/main/java/com/example/chatapplication/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/chatapplication/Adapter/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.example.chatapplication.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.chatapplication.MessageActivity;
 import com.example.chatapplication.Model.User;
 import com.example.chatapplication.R;
 
@@ -35,7 +37,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        User user=mUsers.get(position);
+        final User user=mUsers.get(position);
         holder.username.setText(user.getEmail());
         if(user.getImageURL().equals("default")){
             holder.profile_image.setImageResource(R.mipmap.ic_launcher);
@@ -43,6 +45,14 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
         }else {
             Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
         }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid",user.getId());
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
